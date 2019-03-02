ed607e056898a07c19a04ee2c322bb119df43ccb
saltukkos <saltukkos@gmail.com>
2019-03-02 16:40:05 +0700

    Extract textbox+textblock to separate usercontrol

---
+++ MercurialVS/Saltukkos.MercurialVS.StudioIntegration/Controls/TextBoxWIthHint.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using JetBrains.Annotations;
+
+namespace Saltukkos.MercurialVS.StudioIntegration.Controls
+{
+    public partial class TextBoxWithHint : INotifyPropertyChanged
+    {
+        [NotNull]
+        public static readonly DependencyProperty HintProperty = 
+            DependencyProperty.Register(nameof(Hint), typeof(string), typeof(TextBoxWithHint), new PropertyMetadata(string.Empty));
+
+        [NotNull]
+        public static readonly DependencyProperty TextProperty = 
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(TextBoxWithHint), new PropertyMetadata(string.Empty, TextChangedCallBack));
+
+        private static void TextChangedCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as TextBoxWithHint)?.PropertyChanged?.Invoke(d, new PropertyChangedEventArgs(nameof(Text)));
+        }
+
+        public TextBoxWithHint()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public string Hint
+        {
+            get => (string)GetValue(HintProperty);
+            set => SetValue(HintProperty, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
