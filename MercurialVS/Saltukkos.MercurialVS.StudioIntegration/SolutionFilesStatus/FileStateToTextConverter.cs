c0a835b08372ada82d9d31626bce6aef520c0dde
saltukkos <saltukkos@gmail.com>
2019-10-06 20:53:27 +0700

    MVS-107 improve status view

---
+++ MercurialVS/Saltukkos.MercurialVS.StudioIntegration/SolutionFilesStatus/FileStateToTextConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Saltukkos.MercurialVS.HgServices;
+
+namespace Saltukkos.MercurialVS.StudioIntegration.SolutionFilesStatus
+{
+    public class FileStateToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is FileStatus fileStatus))
+            {
+                return null;
+            }
+
+            switch (fileStatus)
+            {
+                case FileStatus.Unknown:
+                    return "?";
+                case FileStatus.Modified:
+                    return "M";
+                case FileStatus.Added:
+                    return "A";
+                case FileStatus.Removed:
+                    return "R";
+                case FileStatus.Clean:
+                    return "C";
+                case FileStatus.Missing:
+                    return "!";
+                case FileStatus.Ignored:
+                    return "I";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), fileStatus, string.Empty);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
